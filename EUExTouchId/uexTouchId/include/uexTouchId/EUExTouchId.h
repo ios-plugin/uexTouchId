19c1c8cd68ae2422c8dba1460faf86e6ed2c5300
Xerxess <appcan_xrg@163.com>
2015-09-22 15:40:58 +0800

    initial
    initial

---
+++ EUExTouchId/uexTouchId/include/uexTouchId/EUExTouchId.h
@@ -0,0 +1,16 @@
+//
+//  EUExTouchId.h
+//  EUExTouchId
+//
+//  Created by xrg on 15/9/19.
+//  Copyright (c) 2015年 ray_MGL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "EUExBase.h"
+#import "EUExBaseDefine.h"
+#import "EUtility.h"
+
+@interface EUExTouchId : EUExBase
+
+@end
