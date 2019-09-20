b9b567c6809b8abecc81bf23cbff37f6c12c498f
张一鹏 <yipeng.zhang@zymobi.com>
2019-09-20 18:05:28 +0800

    适配iOS13回调问题，防止闪退

--- EUExTouchID/EUExTouchID/EUExTouchID.m
+++ EUExTouchID/EUExTouchID/EUExTouchID.m
@@ -49,9 +49,9 @@ static const NSInteger kUexTouchIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAv
     NSString *hint = stringArg(info[@"hint"]);
     NSNumber *mode = numberArg(info[@"mode"]);
     
-    
+    __weak typeof (self) weakSelf = self;
     void (^callback)(NSInteger resultCode) = ^(NSInteger resultCode){
-        [cb executeWithArguments:ACArgsPack(@(resultCode))];
+        [weakSelf jsCallbackExecuteByMainThread:cb withArguments:ACArgsPack(@(resultCode))];
     };
     
     if (!NSClassFromString(@"LAContext")){
@@ -87,8 +87,9 @@ static const NSInteger kUexTouchIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAv
 -(void) verify :(NSMutableArray*)inArguments{
     ACArgsUnpack(NSString *hint) = inArguments;
     LAContext* context = [[LAContext alloc] init];
+    __weak typeof (self) weakSelf = self;
     void (^callback)(BOOL result,NSString *reason) = ^(BOOL result,NSString *reason){
-        [self.webViewEngine callbackWithFunctionKeyPath:@"uexTouchID.cbVerify" arguments:ACArgsPack(@(result),reason)];
+        [weakSelf callbackWithFunctionKeyPathByMainThread:@"uexTouchID.cbVerify" arguments:ACArgsPack(@(result),reason)];
     };
     
 
@@ -110,6 +111,37 @@ static const NSInteger kUexTouchIDNotAvailable = -6; // -6 = LAErrorTouchIDNotAv
 }
 
 
+#pragma mark - Callback Common
 
+/**
+ 保证在主线程中完成JS回调操作
+ 
+ @param jsFunc 回调JS方法
+ @param args 参数
+ */
+- (void)jsCallbackExecuteByMainThread:(ACJSFunctionRef *)jsFunc withArguments:(NSArray *)args {
+    if([NSThread isMainThread]){
+        [jsFunc executeWithArguments:args];
+    } else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [jsFunc executeWithArguments:args];
+        });
+    }
+}
+
+/**
+ 保证在主线程中完成JS回调操作
+ 
+ @param jsString 回调需要执行的JS字符串
+ */
+- (void)callbackWithFunctionKeyPathByMainThread:(NSString *)JSKeyPath arguments:(nullable NSArray *)arguments {
+    if([NSThread isMainThread]){
+        [self.webViewEngine callbackWithFunctionKeyPath:JSKeyPath arguments:arguments];
+    } else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.webViewEngine callbackWithFunctionKeyPath:JSKeyPath arguments:arguments];
+        });
+    }
+}
 
 @end
